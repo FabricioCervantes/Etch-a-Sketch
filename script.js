9aa995fc8750b18261320e43df8073af6fb94ec6
Fabricio-182 <marcofabri6@gmail.com>
2022-04-10 18:08:34 -0500

    feat: Add option to select a red color

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const container = document.querySelector(".container")
 const btnerase = document.querySelector(".erase")
+const btnred = document.querySelector(".red")
+const btnrainbow = document.querySelector(".rainbow")
+const DEFAULT_COLOR = 'rainbow'
+const DEFAULT_MODE = 'color'
+const DEFAULT_SIZE = 16
+
+let currentColor = DEFAULT_COLOR
+let currentMode = DEFAULT_MODE
+let currentSize = DEFAULT_SIZE
+
+
+function setCurrentColor(newColor) {
+    currentColor = newColor
+}
 
 
 let mouseDown = false
@@ -9,7 +23,8 @@ document.body.onmouseup = () => (mouseDown = false)
 
 grid()
 btnerase.onclick = () => clear();
-
+btnred.onclick = () => setCurrentColor('red')
+btnrainbow.onclick = () => setCurrentColor('rainbow')
 
 function grid() {
     for (let i = 0; i < 256; i++) {
@@ -19,20 +34,25 @@ function grid() {
         square.classList = "gridElements"
         square.addEventListener("mouseover", colorpicker)
         square.addEventListener("mousedown", colorpicker)
-
         container.append(square);
     }
 }
 
-
 function colorpicker(e) {
+
     if (e.type === 'mouseover' && !mouseDown) return
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+    if (currentColor === 'rainbow') {
+        let letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+        e.target.style.backgroundColor = color
     }
-    e.target.style.backgroundColor = color
+    if (currentColor === 'red') {
+        e.target.style.backgroundColor = "red"
+    }
+
 
 }
 
